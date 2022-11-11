e32ebb24ed35230e90792f9949b5be913673ddf7
Ummi Qalsum <ummiqalsum50@gmail.com>
2022-11-12 00:27:08 +0800

    Menambahkan kode program tombol keluar

--- src/gui/PertambahanDuaAngka.java
+++ src/gui/PertambahanDuaAngka.java
@@ -149,6 +149,11 @@ public class PertambahanDuaAngka extends javax.swing.JFrame {
         });
 
         keluarBtn.setText("Keluar");
+        keluarBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                keluarBtnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -234,6 +239,11 @@ public class PertambahanDuaAngka extends javax.swing.JFrame {
         
         angkaPertamaTF.requestFocus();
     }//GEN-LAST:event_hapusBtnActionPerformed
+
+    private void keluarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_keluarBtnActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_keluarBtnActionPerformed
 
     /**
      * @param args the command line arguments
